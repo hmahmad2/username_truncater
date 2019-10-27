3c42fbb76c2fe3f658a8a85d1ae1273a335e48e2
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-10-27 11:45:22 -0500

    javascript truncator function working to a degree. Further testing required

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
-// function truncate (str, num) {
-//     if (str.length <= num) {
-//         return str;
-//     } else {
-//         return str.slice(0, num);
-//     }
-// }
+function truncateHelper (str, num) {
+    if (str.length <= num) {
+        return str;
+    } else {
+        return str.slice(0, num);
+    }
+}
+
+let user_orig;
+let user_num;
+let trunc;
 
 function truncate () {
-    var user_orig = document.getElementById('user').value;
+    user_orig = document.getElementById('user').value;
+    if (user_orig.length === 0) {
+        alert("Please insert a valid username");
+    }
+    user_num = document.getElementById('num').value;
+    if (isNaN(user_num)) {
+        alert("Please insert a valid username length");
+    }
+
+    console.log(user_orig);
+    console.log(user_num);
+
+    trunc = truncateHelper(user_orig, user_num);
+    document.getElementById('answer').innerHTML += trunc;
 }
 
 // var steve = "they call me failcop"
